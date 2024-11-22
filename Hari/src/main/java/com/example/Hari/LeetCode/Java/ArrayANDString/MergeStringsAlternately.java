eb1a878b7810240796259f1188467b7b666085f0
Hari-48 <suthan9850@gmail.com>
2024-11-22 22:21:38 +0530

    file name changed

--- Hari/src/main/java/com/example/Hari/LeetCode/Java/ArrayANDString/MergeStringsAlternately.java
+++ Hari/src/main/java/com/example/Hari/LeetCode/Java/ArrayANDString/MergeStringsAlternately.java
@@ -1,2 +1,31 @@
-package com.example.Hari.LeetCode.Java.ArrayANDString;public class MergeStringsAlternately {
+package com.example.Hari.LeetCode.Java.ArrayANDString;
+
+import java.util.stream.Stream;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+        mergeAlternately("abc", "pqrs");
+    }
+
+
+    public static String mergeAlternately(String word1, String word2) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() || j < word2.length()) {
+
+            if (i < word1.length()) {
+                stringBuilder.append(word1.charAt(i));
+                i++;
+            }
+            if (j < word2.length()) {
+                stringBuilder.append(word2.charAt(i));
+                j++;
+            }
+        }
+        System.out.println(stringBuilder);
+        return String.valueOf(stringBuilder);
+
+    }
 }
