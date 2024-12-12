fac56186f740a0cc454c527295be635aa9a3efb9
Hari-0225 <harihara.sudhan@finsurge.tech>
2024-12-12 09:22:50 +0530

    Quiz Controller - v4

--- Quiz/quizapp/src/main/java/com/hari/quizapp/Controller/QuizController.java
+++ Quiz/quizapp/src/main/java/com/hari/quizapp/Controller/QuizController.java
@@ -1,37 +1,20 @@
 package com.hari.quizapp.Controller;
 
-import com.hari.quizapp.Entity.Question;
-import com.hari.quizapp.Service.QuestionService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("question")
-@Slf4j
+@RequestMapping("/quiz")
 public class QuizController {
 
-    @Autowired
-    QuestionService questionService;
-    @GetMapping("/allQuestion")
-    public ResponseEntity<List<Question>> getAllQuestion() {
-        return questionService.getAllQuestion();
+
+    @PostMapping("/create")
+
+    public ResponseEntity<String> createQuiz(@RequestParam String category ,@RequestParam String numQ , @RequestParam String title ){
+        return new ResponseEntity<>("Hi", HttpStatus.OK);
     }
-
-    @GetMapping("/category/{category}")
-    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
-        return questionService.getQuestionByCategory(category);
-    }
-
-    //save All Question
-    @PostMapping("/save")
-    public ResponseEntity<?> saveQuestions(@RequestBody List<Question> questions){
-        return questionService.saveQuestions(questions);
-    }
-
-
 }
