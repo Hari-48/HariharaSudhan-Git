2467d9de402e10e3681032e623cb317dfdb192a4
Hari-0225 <harihara.sudhan@finsurge.tech>
2024-12-12 10:36:18 +0530

    Get Quiz Questions For user-V6

--- Quiz/quizapp/src/main/java/com/hari/quizapp/Controller/QuizController.java
+++ Quiz/quizapp/src/main/java/com/hari/quizapp/Controller/QuizController.java
@@ -1,14 +1,15 @@
 package com.hari.quizapp.Controller;
 
+import com.hari.quizapp.Model.QuestionWrapper;
 import com.hari.quizapp.Service.QuizService;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.ManyToMany;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -21,6 +22,12 @@ public class QuizController {
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category ,@RequestParam int numQ , @RequestParam String title ){
         return quizService.createQuiz(category,numQ,title);
+    }
+
+
+    @GetMapping("/getQues/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable Long id){
+        return quizService.getQuizQuestions(id);
 
     }
 }
--- Quiz/quizapp/src/main/java/com/hari/quizapp/Service/QuizService.java
+++ Quiz/quizapp/src/main/java/com/hari/quizapp/Service/QuizService.java
@@ -2,6 +2,7 @@ package com.hari.quizapp.Service;
 
 import com.hari.quizapp.Entity.Question;
 import com.hari.quizapp.Entity.Quiz;
+import com.hari.quizapp.Model.QuestionWrapper;
 import com.hari.quizapp.Repository.QuestionRepo;
 import com.hari.quizapp.Repository.QuizRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class QuizService {
@@ -37,6 +42,26 @@ public class QuizService {
 
 
 
+
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Long id) {
+        Optional<Quiz> quiz = quizRepo.findById(id);
+
+        try {
+            List<Question> questionsFromDB = quiz.get().getQuestions();
+            List<QuestionWrapper> questionForUser = questionsFromDB.stream().map(question -> new QuestionWrapper( question.getId(),
+                    question.getQuestionTitle(),
+                    question.getOption1(),
+                    question.getOption2(),
+                    question.getOption3(),
+                    question.getOption4())).toList();
+            return new ResponseEntity<>(questionForUser,HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<List<QuestionWrapper>>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
 
     }
 }
