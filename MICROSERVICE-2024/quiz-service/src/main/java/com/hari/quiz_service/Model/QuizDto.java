80f4cbc410a430627f0a4322a5f21faab14399f3
Hari-0225 <harihara.sudhan@finsurge.tech>
2024-12-14 13:30:29 +0530

    removed unwanted files

--- MICROSERVICE-2024/quiz-service/src/main/java/com/hari/quiz_service/Model/QuizDto.java
+++
@@ -1,2 +0,0 @@
-package com.hari.quiz_service.Model;public class QuizDto {
-}
--- MICROSERVICE-2024/quiz-service/src/main/java/com/hari/quiz_service/Repository/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hari.quiz_service.Repository;
-
-import com.hari.quizapp.Entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz,Long> {
-}
