c68ce1a042eb8fb9c8d00200598ac851660eab50
Hari-0225 <harihara.sudhan@finsurge.tech>
2024-12-16 20:10:24 +0530

    Hacks-problem

--- Hari/src/main/java/com/example/Hari/SQL/HackerRank.sql
+++ Hari/src/main/java/com/example/Hari/SQL/HackerRank.sql
@@ -124,6 +124,193 @@ SELECT CEIL(AVG(Salary) - AVG(REPLACE(Salary,'0',''))) FROM EMPLOYEES
 
 
 
+SELECT
+  c.company_code,
+  c.founder,
+  COUNT(DISTINCT lm.lead_manager_code),
+  COUNT(DISTINCT sm.senior_manager_code),
+  COUNT(DISTINCT m.manager_code),
+  COUNT(DISTINCT e.employee_code)
+FROM
+  Company c
+  LEFT JOIN LEAD_MANAGER lm on lm.company_code = c.company_code
+  LEFT JOIN SENIOR_MANAGER sm on sm.company_code = c.company_code
+  LEFT JOIN Manager m on m.company_code = lm.company_code
+  LEFT JOIN Employee e on e.company_code = m.company_code
+GROUP BY
+  c.company_code,
+  c.founder
+order by
+  c.company_code
+
+
+--We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+--The Employee table containing employee data for a company is described as follows:
+SELECT
+  (months * salary) AS max_earnings,
+  COUNT(*) AS employee_count
+FROM
+  Employee
+  -- WHERE (months * salary) = (
+  --     SELECT MAX(months * salary)
+  --     FROM Employee
+  -- )
+GROUP BY
+  (months * salary)
+ORDER BY
+  (months * salary) DESC
+LIMIT
+  1;
+
+
+
+
+--Query the following two values from the STATION table:
+--The sum of all values in LAT_N rounded to a scale of  decimal places.
+--The sum of all values in LONG_W rounded to a scale of  decimal places.
+--The STATION table is described as follows:
+SELECT ROUND(SUM(LAT_N),2),ROUND(SUM(LONG_W),2) FROM STATION
+
+--Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to  decimal places.
+--Input Format
+--The STATION table is described as follows:
+SELECT
+  ROUND(
+    SUM(LAT_N),
+    4
+  )
+FROM
+  STATION S
+WHERE
+  S.LAT_N > 38.7880
+  AND S.LAT_N < 137.2345
+
+
+--Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than . Truncate your answer to  decimal places.
+
+SELECT
+  ROUND(
+    MAX(LAT_N),
+    4
+  )
+FROM
+  STATION S
+WHERE
+  S.LAT_N < 137.2345
+GROUP BY
+  S.LAT_N
+ORDER BY
+  S.LAT_N DESC
+LIMIT
+  1
+
+
+--Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+SELECT ROUND(LONG_W,4) FROM STATION WHERE LAT_N < 137.2345 ORDER BY LAT_N DESC LIMIT 1
+
+--Query the smallest Northern Latitude (LAT_N) from STATION that is greater than . Round your answer to  decimal places.
+-- SELECT ROUND(LAT_N,4) FROM STATION WHERE LAT_N > 38.7780 ORDER BY LAT_N LIMIT 1
+SELECT MIN(ROUND(LAT_N,4)) FROM STATION WHERE LAT_N > 38.7780
+
+
+
+--Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than . Round your answer to  decimal places.
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N = (
+    SELECT MIN(LAT_N)
+    FROM STATION
+    WHERE LAT_N > 38.7780
+);
+
+--
+--Consider  and  to be two points on a 2D plane.
+-- happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+-- happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+-- happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+-- happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+--Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+SELECT ROUND(((MAX(LAT_N)-MIN(LAT_N))+(MAX(LONG_W)-MIN(LONG_W))),4)FROM STATION
+
+--Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+--Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2)), 4)
+FROM STATION;
+
+--A median is defined as a number separating the higher half of a data set from the lower half. Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+SELECT ROUND( AVG(lat_n), 4 ) AS median
+FROM ( SELECT lat_n, ROW_NUMBER() OVER ( ORDER BY lat_n ) AS rn FROM station ) AS subq
+ WHERE rn = ( SELECT CEIL( ( COUNT(rn)+ 1 )/ 2 ) FROM station ) OR rn = ( SELECT FLOOR( ( COUNT(rn)+ 1 )/ 2 ) FROM station )
+
+--Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT SUM(C1.POPULATION)
+FROM CITY C1
+LEFT JOIN COUNTRY C2 ON C1.COUNTRYCODE = C2.CODE
+WHERE C2.CONTINENT = 'Asia';
+
+--Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT c.name FROM CITY C LEFT JOIN COUNTRY T ON C.COUNTRYCODE = T.CODE WHERE T.CONTINENT ='Africa'
+
+
+
+--Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT
+    C2.CONTINENT,
+    FLOOR(AVG(C1.POPULATION)) AS AVG_CITY_POPULATION
+FROM
+    CITY C1
+ JOIN
+    COUNTRY C2
+ON
+    C1.COUNTRYCODE = C2.CODE
+GROUP BY
+    C2.CONTINENT;
+
+
+Select H.hacker_id , H.name from Submissions S
+Left Join Challenges C on S.challenge_id = C.challenge_id
+Left Join Difficulty D on C.difficulty_level = D.difficulty_level
+Left join Hackers H on S.hacker_id = H.hacker_id
+where S.score = D.score
+Group by H.hacker_id,H.name Having Count(D.score)>1 Order by Count(D.score) DESC , H.hacker_id;
+
+-- Certificate 1 :-
+SELECT s.roll_number,s.name FROM student_information s
+LEFT JOIN faculty_information f1 ON f1.employee_id = s.advisor
+WHERE
+ (f1.gender = 'F' AND f1.salary > 20000)
+    OR
+    (f1.gender = 'M' AND f1.salary > 15000);
+
+SELECT C.customer_id,C.name,CONCAT('+',c1.country_code,C.phone_number) FROM customers C
+LEFT JOIN country_codes c1 ON c1.country = C.country
+ORDER BY C.customer_id
+
+-- Certificate 2
+SELECT c.city_name,p.product_name ,ROUND(ii.line_total_price,2) FROM city c
+JOIN customer c1 on c.id = c1.city_id
+JOIN invoice i on c1.id=i.customer_id
+JOIN invoice_item ii on i.id=ii.invoice_id
+JOIN product p on ii.product_id = p.id
+ORDER BY ii.line_total_price DESC, c.city_name DESC , p.product_name ASC
+
+
+SELECT c.customer_name, i.total_price
+FROM customer c
+JOIN invoice i ON c.id = i.customer_id
+WHERE i.total_price < (
+    SELECT AVG(total_price)
+    FROM invoice
+)
+AND i.total_price < (
+    SELECT SUM(total_price) * 0.25
+    FROM invoice
+);
+
 
 
 
