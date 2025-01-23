36764e39c6599f01bd878a776d23a1bbd2f6cb35
Hari-0225 <harihara.sudhan@finsurge.tech>
2025-01-23 20:58:40 +0530

    jan 23

--- Hari/src/main/java/com/example/Hari/GreeksForGreeks/Node.java
+++ Hari/src/main/java/com/example/Hari/GreeksForGreeks/Node.java
@@ -5,9 +5,13 @@ public class Node {
     int data;
     Node next;
 
+    Node random;
+
     Node(int data) {
         this.data = data;
         this.next = null;
+
+        this.random=null;
     }
 
 }
