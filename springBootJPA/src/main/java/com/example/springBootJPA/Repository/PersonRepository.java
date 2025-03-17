7a1323d5725ab04eb6aa5335c09d73cc02a72a4c
Hari-0225 <harihara.sudhan@finsurge.tech>
2025-03-17 11:53:02 +0530

    removed jpa

--- springBootJPA/src/main/java/com/example/springBootJPA/Repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springBootJPA.Repository;
-
-
-import com.example.springBootJPA.Entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
